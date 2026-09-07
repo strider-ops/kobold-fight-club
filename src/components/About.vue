a4614262425fee7836ac469a3e917bc7d6b3a67b
Robert (Bob) Pierce <ropierce@umich.edu>
2026-09-07 09:21:45 -0400

    fix encounter info styling

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,31 +1,7 @@
 <template>
   <div class="container">
-    <!-- Logo disabled for production build - image path issue -->
-    <!-- <div class="about--logo pull-right">
-      <img src="../../images/logo.png" class="img-responsive" alt="Logo">
-    </div> -->
 
-    <h2>Welcome to Kobold Fight Cemetery, a quick-and-dirty hack to revive the functionality of Kobold Fight Club</h2>
+    <h4>AI / Vue upgrade of original Kobold Fight Club/Cemetary</h4>
 
-    <h3>Original project:</h3>
-    <a href="http://kobold.club/5e-monsters/help.html" target="_blank">Kobold Fight Club</a>
-
-    <h3>Via Reddit</h3>
-    <a href="http://www.reddit.com/r/asmor" target="_blank">Asmor's Official Subreddit</a>
-
-    <h3>Contact the Maintainers Directly</h3>
-    <dl class="dl-horizontal">
-      <dt>Site Owner:</dt>
-      <dd>Max Wilson</dd>
-      <dd><a href="mailto:wilson.max@gmail.com">wilson.max@gmail.com</a></dd>
-    </dl>
-
-    <p class="about--disclaimer lead">
-      Kobold Fight Cemetery is not associated with Wizards of the Coast.
-    </p>
   </div>
 </template>
-
-<script setup>
-// Static page - no logic needed
-</script>
